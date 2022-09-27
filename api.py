23a2dea1423ae6665c7b6fb792ddd992afc8b817
ZennithYattare <luis.espera.0758@gmail.com>
2022-09-27 21:49:59 +0800

    Added gitignore and updated API

---
+++ api.py
@@ -0,0 +1,3 @@
+apiKey = "205ed62a0bee3bf828a4d4d98ecfd1c7"
+
+fieldsResponse = "ip,country_name,city,region_name"
--- ipv4ipv6.py
+++ ipv4ipv6.py
@@ -1,24 +1,26 @@
+
+# Import API access key and specific fields or types of information
+from api import apiKey, fieldsResponse
 # This example requires the requests library be installed.  You can learn more about the Requests library here: https://pypi.org/project/requests/
 from requests import get
 # JSON encoder and decoder information here: https://docs.python.org/3/library/json.html
 import json
 
-# IP-API documentation: https://ip-api.com/docs
-loc = get('http://ip-api.com/json/?fields=8729')
+# IPAPI documentation: https://ipapi.com/documentation
+apiUrl = get(f"http://api.ipapi.com/check?access_key={apiKey}&fields={fieldsResponse}")
 
 # The json.dumps method converts a Python object to a JSON formatted string.
-responseString = json.dumps(loc.json())
+responseString = json.dumps(apiUrl.json())
 
 # Call json.loads(json_object_string) to convert a json_object_string to a dictionary. Use dictionary indexing to access a value associated with a specific key.
 
 # If you need to parse a JSON string into a native Python object, you have to use the json.loads() method--this is now a Python dictionary.
 pythonObject = json.loads(responseString)
 
-# Multiline f-string print the value of the query/isp/city/country key. 
+# Multiline f-string print the value of the ip/city/region/country field. 
 # Read more here: https://docs.python.org/3/whatsnew/3.6.html#whatsnew36-pep498 and https://stackoverflow.com/questions/10112614/how-do-i-create-a-multiline-python-string-with-inline-variables
 print(f"""
-    IPv4/IPv6: {pythonObject['query']}
-    ISP: {pythonObject['isp']}
+    IPv4/IPv6: {pythonObject['ip']}
     City: {pythonObject['city']}
-    Region: {pythonObject['regionName']}
-    Country: {pythonObject['country']}""")
+    Region: {pythonObject['region_name']}
+    Country: {pythonObject['country_name']}""")
