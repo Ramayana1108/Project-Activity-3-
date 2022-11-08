5db797385cf6f04fe1b30d56f35569537d0eb719
ZennithYattare <luis.espera.0758@gmail.com>
2022-11-08 20:18:55 +0800

    install requests and secret API_KEY

--- .github/workflows/preTest.py
+++ .github/workflows/preTest.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-# API_KEY = os.environ["API_KEY"]
+API_KEY = os.environ["API_KEY"]
 
 def test_httpStatusCodeEqual200():
-    response = requests.get("http://api.ipapi.com/check?access_key=205ed62a0bee3bf828a4d4d98ecfd1c7")
+    response = requests.get("http://api.ipapi.com/check?access_key={API_KEY}")
     assert response.status_code == 200
